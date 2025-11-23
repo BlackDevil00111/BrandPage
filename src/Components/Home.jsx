d29f51bfaa7151a0fa3b0a4a280fddae338c8aee
Sujal Mitra <sujalmitra2021@gmail.com>
2025-11-23 23:51:12 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import BrandLogo from '/images/brand_logo.png'
+const Home = () => {
+  return (
+    <div>
+      <nav className="container">
+        <div className="Logo">
+          <img src={BrandLogo} alt="BrandLogo"></img>
+        </div>
+        <ul>
+          <li href="#">Home</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button className="Login">Login</button>
+      </nav>
+      <main className="hero container">
+        <div className="hero-content">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        
+        <div className="hero-btn">
+          <button className="shopnow"> Shop Now</button>
+          <button className="category">Category</button>
+        </div>
+        <div className="hero-logo">
+          <p>Also Available On</p>
+          <div className="hero-logo1">
+          <img className="logoAZ" src="\images\amazon.png" alt="Amazon"></img>
+          <img className="logoFK" src="\images\flipkart.png" alt="Flipkart"></img>
+          </div>
+        </div>
+        </div>
+        <div className="hero-image">
+          <img className="imgShoe" src="\images\shoe_image.png"></img>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
